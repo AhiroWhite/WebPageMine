93694e1ef6c7d63a1a22fec52694c2e70305947b
AhiroWhite <sahsa0207@gmail.com>
2021-04-20 13:58:31 +0300

    add js code
    Add js sript, for persons shower

---
+++ js/index.js
@@ -0,0 +1,102 @@
+
+// Переменные персонала
+var f1 = document.getElementById("f1");
+var f2 = document.getElementById("f2");
+var f3 = document.getElementById("f3");
+var f4 = document.getElementById("f4");
+var f5 = document.getElementById("f5");
+var f6 = document.getElementById("f6");
+var f7 = document.getElementById("f7");
+var f8 = document.getElementById("f8");
+var fcont = document.getElementById("fcont");
+// Функции смена стилей
+function active_1(obj) {
+   f1.style.width = "20%";
+   f1.style.height = "300px";
+   f1.style.transition = "0.2s";
+   f1.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_1(obj) {
+   f1.style.width = "12%";
+   f1.style.height = "196px";
+   f1.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+function active_2(obj) {
+   f2.style.width = "20%";
+   f2.style.height = "300px";
+   f2.style.transition = "0.2s";
+   f2.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_2(obj) {
+   f2.style.width = "12%";
+   f2.style.height = "196px";
+   f2.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+
+function active_3(obj) {
+   f3.style.width = "20%";
+   f3.style.height = "300px";
+   f3.style.transition = "0.2s";
+   f3.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_3(obj) {
+   f3.style.width = "12%";
+   f3.style.height = "196px";
+   f3.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+
+function active_4(obj) {
+   f4.style.width = "20%";
+   f4.style.height = "300px";
+   f4.style.transition = "0.2s";
+   f4.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_4(obj) {
+   f4.style.width = "12%";
+   f4.style.height = "196px";
+   f4.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+function active_5(obj) {
+   f5.style.width = "20%";
+   f5.style.height = "300px";
+   f5.style.transition = "0.2s";
+   f5.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_5(obj) {
+   f5.style.width = "12%";
+   f5.style.height = "196px";
+   f5.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+function active_6(obj) {
+   f6.style.width = "20%";
+   f6.style.height = "300px";
+   f6.style.transition = "0.2s";
+   f6.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_6(obj) {
+   f6.style.width = "12%";
+   f6.style.height = "196px";
+   f6.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+function active_7(obj) {
+   f7.style.width = "20%";
+   f7.style.height = "300px";
+   f7.style.transition = "0.2s";
+   f7.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_7(obj) {
+   f7.style.width = "12%";
+   f7.style.height = "196px";
+   f7.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
+function active_8(obj) {
+   f8.style.width = "20%";
+   f8.style.height = "300px";
+   f8.style.transition = "0.2s";
+   f8.style.backgroundImage = "url('https://sun9-22.userapi.com/impg/ikIzHc7KMLfeNnVKZxkKWQUI6-MWfHeU6Gm3kQ/mNxH_15lTkk.jpg?size=1280x1278&quality=96&sign=3dd8c5af749f1e77ea22a80bf0956451&type=album')";
+}
+function disactive_8(obj) {
+   f8.style.width = "12%";
+   f8.style.height = "196px";
+   f8.style.backgroundImage = "url('https://sun9-5.userapi.com/impg/2nj91u4yDWkCN0soPSq1KKJHO-rvW3311BxM6Q/Dasf6uSKHxY.jpg?size=1280x1278&quality=96&sign=264414c8e308fe69b658dbc40e5a1940&type=album')";
+}
